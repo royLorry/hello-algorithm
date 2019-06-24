a0731ca68ed2e48f19ea5fa3229ec42d60e00de8
LR <luorui3@lenovo.com>
2019-06-24 19:33:17 +0800

    V3

--- 0623/TrappingRainWater.java
+++ 0623/TrappingRainWater.java
@@ -1,44 +1,76 @@
 import java.util.ArrayList;
 
 public class TrappingRainWater {
-    public int trap(int[] height) {
-        int arrayLength = height.length;
-        if (arrayLength <= 1) {
-            return 0;
-        }
-        int maxHeight = height[0];
-        /**
-         * 求出最大高度
-         */
-        for (int i = 0, thisHeight = 0; i < arrayLength; i++) {
-            thisHeight = height[i];
-            if (thisHeight > maxHeight) {
-                maxHeight = thisHeight;
+    private List<Integer> insertNewArrow(List<Integer> sideArrayList,int[] height, int currentArrow){
+        int listLast = sideArrayList.size();
+        int p = listLast - 2;
+        if (listLast > 1 && height[sideArrayList.get(listLast-1)] < height[currentArrow]) {
+            while (p >= 0) {
+                int pValue = height[sideArrayList.get(p)];
+                int i = p + 1;
+                while (i < listLast) {
+                    if (pValue <= height[sideArrayList.get(i)]) {
+                        break;
+                    }
+                    i++;
+                }
+                if (i == listLast) {
+                    sideArrayList = sideArrayList.subList(0, p+1);
+                    listLast = sideArrayList.size();
+                }
+                if (pValue >= height[currentArrow]) {
+                    break;
+                }
+                p--;
             }
         }
-        /**
-         * 从第一层开始往上，记录每一层能有值的下标，这些下标所记录的值之间如有空位必定可以存水
-         */
-        int pool = 0;
-        int temp = 0;
-        for (int i = 1; i <= maxHeight; i++) {
-            for (int j = 0, thisHeight = 0; j < arrayLength; j++) {
-                thisHeight = height[j];
-                if (thisHeight >= i ) {
-                    j++;
-                    temp = 0;
-                    for (; j < arrayLength; j++) {
-                        thisHeight = height[j];
-                        if (thisHeight < i) {
-                            temp++;
-                        } else {
-                            pool += temp;
-                            temp = 0;
-                        }
+        sideArrayList.add(currentArrow);
+        return sideArrayList;
+    }
+
+    public int trap(int[] height) {
+        int arraySize = height.length;
+        if (arraySize < 3) {
+            return 0;
+        }
+        List<Integer> sideArrayList = new ArrayList<Integer>();
+        int before = 0;
+        int current = height[0];
+        int next = 0;
+        if (current > 0) {
+            sideArrayList.add(0);
+        }
+        for (int i = 1, max = arraySize - 1; i < max; i++) {
+            before = height[i -1];
+            current = height[i];
+            next = height[i+1];
+            if (current > before && current >= next){
+                sideArrayList = insertNewArrow(sideArrayList, height, i);
+            }
+        }
+        current = height[arraySize-1];
+        if (current > height[arraySize - 2]) {
+            sideArrayList = insertNewArrow(sideArrayList, height, arraySize-1);
+        }
+        int listLast = sideArrayList.size();
+        if (listLast > 1) {
+            int pool = 0;
+            for (int i = 0, max = listLast - 1, left = 0, right = 0, leftVaule = 0, rightVaule = 0, lowerVaule = 0; i < max; i++) {
+                left = sideArrayList.get(i);
+                right = sideArrayList.get(i+1);
+                leftVaule = height[left];
+                rightVaule = height[right];
+                lowerVaule = leftVaule < rightVaule ? leftVaule : rightVaule;
+                for (int j = left + 1; j < right; j++) {
+                    current = height[j];
+                    if (lowerVaule > current) {
+                        pool += lowerVaule - current;
                     }
                 }
             }
+            return pool;
+        } else {
+            return 0;
         }
-        return pool;
     }
 }
