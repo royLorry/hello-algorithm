cd67ed0f1e834c2d7a9b920ecd0f6cd25ea552a2
LR <luorui3@lenovo.com>
2019-07-07 22:48:59 +0800

    temp

--- 0706/LRUextendArray.java
+++ 0706/LRUextendArray.java
@@ -1,16 +1,24 @@
+import java.util.LinkedHashMap;
+
 public class LRUextendArray {
 
-    private int[] array;
+    private LinkedHashMap linkedHashMap;
 
     public LRUCache(int capacity) {
-        
+        linkedHashMap = new LinkedHashMap<>(capacity);
     }
     
     public int get(int key) {
-        
+        if (linkedHashMap.containsKey(key)) {
+            int value = linkedHashMap.get(key);
+            // 获取值并移动至链表首位
+        } else {
+            return -1;
+        }
     }
     
     public void put(int key, int value) {
-        
+        // 判断存在
+        // 不存则新增,新增时判断是否超过长度,超过则将链表尾部删除
     }
 }
