065d70fd578c7c2cd3445bbe34146cb98a30947d
LR <luorui3@lenovo.com>
2019-07-07 22:38:24 +0800

    temp

---
+++ 0706/LRUextendArray.java
@@ -0,0 +1,16 @@
+public class LRUextendArray {
+
+    private int[] array;
+
+    public LRUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+}
