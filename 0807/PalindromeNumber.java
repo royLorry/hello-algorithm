a99f10eb103ab43296a93fe6c5fd7556d6cc6116
luorui3 <luorui3@lenovo.com>
2019-08-07 15:28:03 +0800

    PalindromeNumber

---
+++ 0807/PalindromeNumber.java
@@ -0,0 +1,43 @@
+public class PalindromeNumber {
+    /**
+     * 官方最优解
+     * @author LeetCode
+     * @Link 
+     * {@link}https://leetcode-cn.com/problems/two-sum/solution/hui-wen-shu-by-leetcode/
+     */
+    public bool IsPalindromeOfficial(int x) {
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+        // 则其第一位数字也应该是 0
+        // 只有 0 满足这一属性
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int div = 1;
+        while (x / div >= 10) div *= 10;
+        while (x > 0) {
+            int left = x / div;
+            int right = x % 10;
+            if (left != right) return false;
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
+    }
+}
