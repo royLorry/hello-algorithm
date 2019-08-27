ad9af9e9211412ad29ae8a3d91ef007c3bb8f6ee
luorui3 <luorui3@lenovo.com>
2019-08-27 14:06:12 +0800

    RemoveElement2

--- 0815/RemoveElement.java
+++ 0815/RemoveElement.java
@@ -1,4 +1,31 @@
 public class RemoveElement {
+
+    public int official(int[] nums, int val){
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                // reduce array size by one
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+
     public int removeElement(int[] nums, int val) {
         int i = 0;
         int j = nums.length - 1;
