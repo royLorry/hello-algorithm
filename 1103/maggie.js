5e99f8536a07b0ad4628317e0f9350e649ceafc2
Shuo Zhang <523758718@qq.com>
2019-11-05 20:20:26 +0800

    Update maggie.js

--- 1103/maggie.js
+++ 1103/maggie.js
@@ -7,3 +7,40 @@ var canJump = function(nums) {
     }
     return last == 0;
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    const len = nums.length;
+    let start = 0;
+    let end = nums[0];
+    let max = nums[0];
+    if(nums.length<2){
+        return true;
+    }
+    if(nums[0]==0){
+        return false;
+    }
+    while(max<len-1){
+        let n = start;
+        let m = end;
+        start = end;
+        //console.log(n,m)
+        for(var i=n;i<=m;i++){
+            if(i+nums[i]>max){
+                max = i+nums[i];
+                end = max;
+                //console.log(end)
+            }
+        }
+        if(end<len&&end<=m&&nums[m]==0){
+            //console.log(end)
+            return false;
+        }
+        
+    }
+    return true;
+};
